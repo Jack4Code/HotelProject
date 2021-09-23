d199933d7dfe39c178070432c1b37630ef863f55
jake <jakeformico@gmail.com>
2021-09-23 14:14:30 -0500

    added a test comment

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -8,6 +8,9 @@ public class Main {
     public static void main(String[] args) {
         boolean validUser = SqlConnection.validateUser("jack@giannini.org", "testPassword1!");
         System.out.println(validUser ? "User found!" : "Not a valid user password combination!");
+
+
+        //test - Jake
     }
 
 }
