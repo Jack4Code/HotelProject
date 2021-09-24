9e0bc6b18094218683133f9dea8120afb2ad2205
Jack4Code <jack.giannini@gmail.com>
2021-09-23 20:41:58 -0400

    Modified overall structure. Added enum. Seperated concerns into different folders.

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -1,13 +1,19 @@
 package main.java;
 
-
 import main.java.DataModels.User;
+import main.java.Utilities.SqlConnection;
 
 public class Main {
 
     public static void main(String[] args) {
-        boolean validUser = SqlConnection.validateUser("jack@giannini.org", "testPassword1!");
-        System.out.println(validUser ? "User found!" : "Not a valid user password combination!");
+
+//        User user = new User("Susie", "Cue", "susie.cue@gmail.com");
+//        user.password = "testPassword1!";
+//
+//        SqlConnection.createUser(user);
+//        SqlConnection.validateUserCredentials(user);
+//        SqlConnection.getUserType(user.id);
+
     }
 
 }
