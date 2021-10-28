23ca9941d267612ed50f3ba6de1c95e8a8713c8c
Jack Giannini <jack.giannini@gmail.com>
2021-10-27 21:24:31 -0400

    stubbed out some more methods

--- src/main/java/main/java/Managers/ReservationManager.java
+++ src/main/java/main/java/Managers/ReservationManager.java
@@ -13,19 +13,20 @@ public class ReservationManager {
         //Get all rooms
         ArrayList<Room> allRooms = SqlConnection.getAllRooms();
         ArrayList<Room> availableRooms = new ArrayList<>();
-
         //Get all available rooms
         for (int i = 0; i < allRooms.size(); i++){
             if (allRooms.get(i).isAvailable == 1){
                 availableRooms.add(allRooms.get(i));
             }
         }
-
         return availableRooms;
-
     }
 
+    //getAllAvailableRoomsByDateRange(DateTime fromDate, DateTime toDate) { //returns list of available rooms available at that time. It will call SqlConnection method }
 
+    //makeReservation(fromDate, toDate, ) returns String: reservationCode ... needs coresponding SqlConnection method
+
+    //upateReservation(reservationCode, whatever we can update) ... needs corresponding SqlConnection method
 
 
 }
